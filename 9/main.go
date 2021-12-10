66576e0b6b98b8ebb33826c5ef5b83c9080d9225
Kevin Bouwkamp <kevin@cohese.net>
2021-12-09 20:29:44 -0500

    day 9 complete

--- 9/main.go
+++ 9/main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type Table struct {
-	data   string
-	width  int
-	length int
+	data          string
+	width         int
+	length        int
+	sprawlChecked [][2]int
 }
 
 func (t *Table) cellAt(x int, y int) int {
@@ -37,6 +39,60 @@ func (t *Table) isLow(x int, y int) bool {
 		cell < t.cellAt(x-1, y)
 }
 
+func (t *Table) hasSprawlChecked(x int, y int) bool {
+	for _, coords := range t.sprawlChecked {
+		if coords[0] == x && coords[1] == y {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (t *Table) basinSize(x int, y int) int {
+	t.sprawlChecked = [][2]int{{x, y}}
+	size := 1
+	// sprawl up
+	size += t.sprawl(x, y-1, "up")
+	// sprawl right
+	size += t.sprawl(x+1, y, "right")
+	// sprawl down
+	size += t.sprawl(x, y+1, "down")
+	// sprawl left
+	size += t.sprawl(x-1, y, "left")
+	return size
+}
+
+func (t *Table) sprawl(x int, y int, dir string) int {
+	if t.hasSprawlChecked(x, y) {
+		return 0
+	}
+	t.sprawlChecked = append(t.sprawlChecked, [2]int{x, y})
+
+	cell := t.cellAt(x, y)
+	if cell >= 9 {
+		return 0
+	}
+	size := 1
+	if dir != "down" {
+		// up
+		size += t.sprawl(x, y-1, "up")
+	}
+	if dir != "right" {
+		// left
+		size += t.sprawl(x-1, y, "left")
+	}
+	if dir != "left" {
+		// right
+		size += t.sprawl(x+1, y, "right")
+	}
+	if dir != "up" {
+		// down
+		size += t.sprawl(x, y+1, "down")
+	}
+	return size
+}
+
 func main() {
 	var table *Table
 	// read input from txt
@@ -52,6 +108,8 @@ func main() {
 		length: length,
 	}
 
+	lows := [][2]int{}
+
 	///////////////////////////////////////////
 	// challenge 1
 	//
@@ -59,10 +117,23 @@ func main() {
 	for x := 0; x < width; x++ {
 		for y := 0; y < length; y++ {
 			if table.isLow(x, y) {
+				lows = append(lows, [2]int{x, y})
 				riskSum += 1 + table.cellAt(x, y) // 1 plus its height
 			}
 		}
 	}
 
 	fmt.Println(riskSum)
+
+	////////////////////////////////////////////
+	// challenge 2
+	//
+	basinSizes := []int{}
+	for _, low := range lows {
+		basinSizes = append(basinSizes, table.basinSize(low[0], low[1]))
+	}
+	sort.Ints(basinSizes)
+
+	// multiply top 3 biggest basins
+	fmt.Println(basinSizes[len(basinSizes)-1] * basinSizes[len(basinSizes)-2] * basinSizes[len(basinSizes)-3])
 }
