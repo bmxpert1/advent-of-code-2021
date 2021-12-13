c0d17da87b8edaed0d599dce62b5ecd4acc8d73d
Kevin Bouwkamp <kevin@cohese.net>
2021-12-12 23:21:51 -0500

    day 12

--- 12/main.go
+++ 12/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 type Node struct {
@@ -29,6 +28,41 @@ func (n *Node) addConnection(targetNode *Node, addReverse bool) {
 	}
 }
 
+func findPaths(nodeMap map[string]*Node, specialNode *Node) (paths []string) {
+	start, end := nodeMap["start"], nodeMap["end"]
+
+	for _, node := range nodeMap {
+		node.visitCount = 0
+	}
+
+	return start.pathsToTarget(end, specialNode)
+}
+
+func (n *Node) pathsToTarget(targetNode *Node, specialNode *Node) (paths []string) {
+	n.visitCount++
+
+	for _, conn := range n.connections {
+		path := n.name
+		if conn.targetNode.isSmall() &&
+			((conn.targetNode != specialNode && conn.targetNode.visitCount > 0) ||
+				(conn.targetNode == specialNode && conn.targetNode.visitCount > 1)) {
+			// we can no longer visit this node
+			continue
+		} else {
+			if conn.targetNode == targetNode {
+				paths = append(paths, path+","+targetNode.name)
+			} else {
+				for _, p := range conn.targetNode.pathsToTarget(targetNode, specialNode) {
+					paths = append(paths, path+","+p)
+				}
+			}
+		}
+	}
+	n.visitCount--
+
+	return paths
+}
+
 type Connection struct {
 	sourceNode *Node
 	targetNode *Node
@@ -36,19 +70,19 @@ type Connection struct {
 
 func main() {
 	// read input from txt
-	paths := []string{}
-	file, _ := os.Open("simple_example_input.txt")
+	connections := []string{}
+	file, _ := os.Open("input.txt")
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		paths = append(paths, scanner.Text())
+		connections = append(connections, scanner.Text())
 	}
 
 	nodeMap := map[string]*Node{}
 
-	for _, path := range paths {
-		parts := strings.Split(path, "-")
+	for _, conn := range connections {
+		parts := strings.Split(conn, "-")
 		start, end := parts[0], parts[1]
 
 		if nodeMap[start] == nil {
@@ -62,5 +96,32 @@ func main() {
 		nodeMap[start].addConnection(nodeMap[end], true)
 	}
 
-	spew.Dump(nodeMap["b"])
+	//////////////////////////////////
+	// challenge 1
+	//
+	fmt.Println(len(findPaths(nodeMap, nil)))
+
+	//////////////////////////////////
+	// challenge 2
+	//
+	paths := []string{}
+	smalls := []*Node{}
+	for _, node := range nodeMap {
+		if node.isSmall() && node.name != "start" && node.name != "end" {
+			smalls = append(smalls, node)
+		}
+	}
+	for _, small := range smalls {
+		paths = append(paths, findPaths(nodeMap, small)...)
+	}
+	uniqPaths := map[string]string{}
+	for _, path := range paths {
+		uniqPaths[path] = path
+	}
+	paths = []string{}
+	for path := range uniqPaths {
+		paths = append(paths, path)
+	}
+
+	fmt.Println(len(paths))
 }
