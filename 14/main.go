844f647e5e9badc491b3adc71ae52d376943a0ce
Kevin Bouwkamp <kevin@cohese.net>
2021-12-14 14:42:31 -0500

    WIP high difficulty

--- 14/main.go
+++ 14/main.go
@@ -3,12 +3,86 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strings"
 )
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+type Pair struct {
+	orig  string
+	value string
+}
+
+func run(template string, steps int, mods *map[string]string) string {
+	pairs := []*Pair{}
+
+	for i := 0; i < len(template)-1; i++ {
+		pairs = append(pairs, &Pair{template[i : i+2], template[i : i+2]})
+	}
+
+	// targetCount := len(pairs)
+	// for i := 0; i < steps-1; i++ {
+	// 	targetCount += targetCount
+	// }
+
+	// fmt.Printf("target count: %v\n", targetCount)
+
+	final := ""
+	// currentCount := 0
+
+	for i, pair := range pairs {
+		fmt.Println(i)
+		// final += pair.runPair(mods, targetCount, &currentCount, i == 0)
+		fmt.Printf("running pair: %v\n", pair.orig)
+		supplement := pair.runPair(mods, steps, 0, i == 0)
+		fmt.Printf("finished pair: %v, %v\n", pair.orig, supplement)
+		final += supplement
+	}
+
+	return final
+}
+
+// func (p *Pair) runPair(mods *map[string]string, targetCount int, currentCount *int, first bool) string {
+func (p *Pair) runPair(mods *map[string]string, targetSteps int, currentStep int, first bool, aggString string) string {
+	if currentStep >= targetSteps {
+		return ""
+	}
+
+	fmt.Printf("running inner pair: %v, %v, step: %v\n", p.orig, first, currentStep)
+	final := ""
+
+	p.inject(mods)
+	final = p.value
+
+	for i, pair := range p.split() {
+		supplement := pair.runPair(mods, targetSteps, currentStep+1, first && i == 0)
+		final += supplement
+		if p.orig == "NN" {
+			fmt.Printf("*** %v [%v]\n", supplement, final)
+		}
+	}
+
+	if !first && len(final) > 0 {
+		final = final[1:]
+	}
+
+	fmt.Printf("finished inner pair: %v, %v\n", p.orig, final)
+
+	return final
+}
+
+func (p *Pair) inject(mods *map[string]string) {
+	p.value = string(p.value[0]) + (*mods)[p.value] + string(p.value[1])
+}
+
+func (p *Pair) split() (pairs [2]*Pair) {
+	for i := 0; i < len(p.value)-1; i++ {
+		pairs[i] = &Pair{p.value[i : i+2], p.value[i : i+2]}
+	}
+
+	return pairs
+}
 
 func main() {
 	file, _ := os.Open("example_input.txt")
@@ -34,68 +108,77 @@ func main() {
 	//
 	currentTemplate := template
 
-	for step := 0; step < 10; step++ {
-		// generate pairs to lookup
-		pairs := []string{}
+	fmt.Println(run(currentTemplate, 2, &mods))
 
-		for i := 0; i < len(currentTemplate)-1; i++ {
-			pairs = append(pairs, currentTemplate[i:i+2])
-		}
+	// for step := 0; step < 10; step++ {
+	// 	// generate pairs to lookup
+	// 	pairs := []string{}
 
-		newTemplate := ""
+	// 	for i := 0; i < len(currentTemplate)-1; i++ {
+	// 		pairs = append(pairs, currentTemplate[i:i+2])
+	// 	}
 
-		for i, pair := range pairs {
-			if i == 0 {
-				newTemplate += string(pair[0])
-			}
-			newTemplate += mods[pair] + string(pair[1])
-		}
+	// 	newTemplate := ""
 
-		currentTemplate = newTemplate
-	}
+	// 	for i, pair := range pairs {
+	// 		if i == 0 {
+	// 			newTemplate += string(pair[0])
+	// 		}
+	// 		newTemplate += mods[pair] + string(pair[1])
+	// 	}
 
-	countMap := map[string]int{}
-	countMin, countMax := math.MaxInt64, math.MinInt64
+	// 	currentTemplate = newTemplate
+	// }
 
-	for _, ltr := range strings.Split(letters, "") {
-		countMap[ltr] = strings.Count(currentTemplate, ltr)
-		if countMap[ltr] < countMin && countMap[ltr] != 0 {
-			countMin = countMap[ltr]
-		}
-		if countMap[ltr] > countMax {
-			countMax = countMap[ltr]
-		}
-	}
+	// countMap := map[string]int{}
+	// countMin, countMax := math.MaxInt64, math.MinInt64
 
-	fmt.Println(countMax - countMin)
+	// for _, ltr := range strings.Split(letters, "") {
+	// 	countMap[ltr] = strings.Count(currentTemplate, ltr)
+	// 	if countMap[ltr] < countMin && countMap[ltr] != 0 {
+	// 		countMin = countMap[ltr]
+	// 	}
+	// 	if countMap[ltr] > countMax {
+	// 		countMax = countMap[ltr]
+	// 	}
+	// }
+
+	// fmt.Println(countMax - countMin)
 
 	///////////////////////////////
 	// challenge 2
 	//
 	// same thing but do it 40 times
-	currentTemplate = template
+	// currentTemplate = template
 
-	for step := 0; step < 40; step++ {
-		fmt.Println(step)
+	// for step := 0; step < 40; step++ {
+	// 	fmt.Printf("%v A\n", step)
 
-		// generate pairs to lookup
-		pairs := make([]string, len(currentTemplate)-1)
+	// 	// generate pairs to lookup
+	// 	pairs := make([]string, len(currentTemplate)-1)
 
-		for i := 0; i < len(currentTemplate)-1; i++ {
-			pairs[i] = currentTemplate[i : i+2]
-		}
+	// 	for i := 0; i < len(currentTemplate)-1; i++ {
+	// 		pairs[i] = currentTemplate[i : i+2]
+	// 	}
 
-		newTemplate := ""
+	// 	fmt.Printf("%v B, %v\n", step, len(pairs))
 
-		for i, pair := range pairs {
-			if i == 0 {
-				newTemplate += string(pair[0])
-			}
-			newTemplate += mods[pair] + string(pair[1])
-		}
+	// 	parts := make([]string, len(pairs))
 
-		currentTemplate = newTemplate
-	}
+	// 	for i, pair := range pairs {
+	// 		supplement := ""
+	// 		if i == 0 {
+	// 			supplement += string(pair[0])
+	// 		}
+	// 		supplement += mods[pair] + string(pair[1])
+
+	// 		parts[i] = supplement
+	// 	}
+
+	// 	fmt.Printf("%v C, %v\n", step, uintptr(len(pairs))*reflect.TypeOf(pairs).Elem().Size())
+
+	// 	currentTemplate = strings.Join(parts, "")
+	// }
 
 	// countMap = map[string]int{}
 	// countMin, countMax = math.MaxInt64, math.MinInt64
